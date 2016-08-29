488ea54b53431fa18146363d57ca674cb89ade8b
Jacob McKay <Jacob McKay>
2016-08-28 19:44:35 -0500

    Hook up Will's animations to WASD for demo

---
+++ Super-Gladiator-Burner-3000-BC-3D-en-LD36/Assets/Programming/GladiatorController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GladiatorController : MonoBehaviour {
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+	    _animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W)){
+            _animator.SetTrigger("EnteredArena");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _animator.SetTrigger("SetOnFire");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _animator.SetTrigger("Die");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _animator.SetTrigger("Extinguished");
+        }
+    }
+}
